66b4e2ca67e5fc4ef91c68a01d15f8b1d1eaa927
Charles Wesley Cho <charleswcho@gmail.com>
2015-11-12 12:16:36 -0800

    First commit

---
+++ DDG-Search/Pods/Target Support Files/Pods-DDG-Search/Pods-DDG-Search-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DDG_Search : NSObject
+@end
+@implementation PodsDummy_Pods_DDG_Search
+@end
---
+++ DDG-Search/Pods/Target Support Files/Pods-DDG-Search/Pods-DDG-Search-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_DDG_SearchVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DDG_SearchVersionString[];
+
